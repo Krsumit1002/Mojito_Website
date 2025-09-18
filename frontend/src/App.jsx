5ae3cc4690e7af2c83c3dfe5b9d298e497bbdb79
sumit kumar <krsumit1002@gmail.com>
2025-09-18 10:05:29 +0530

    final commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,27 @@
 import {ScrollTrigger ,SplitText} from "gsap/all"
 import gsap from 'gsap'
 import React from 'react'
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Cocktails from "./components/Cocktails"
+import About from "./components/About"
+import Art from "./components/Art"
+import Menu from "./components/Menu"
+import Contact from "./components/Contact"
 
 gsap.registerPlugin(ScrollTrigger,SplitText)
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      hello 
-    </div>
+    <main>
+      <Navbar/>
+      <Hero/>
+      <Cocktails/>
+      <About/>
+      <Art/>
+      <Menu/>
+      <Contact/>
+    </main>
   )
 }
 
